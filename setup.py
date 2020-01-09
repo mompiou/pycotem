f0d998104480f3ba4e438c1c314a81a972a7506f
mompiou <mompiou@cemes.fr>
2020-01-09 16:36:55 +0100

    pycotem 1.1.0

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open(path.join(this_directory, 'README.md')) as f:
 setup(
     name = 'pycotem',
     packages=find_packages(exclude=("doc","test")),
-    version = '1.0.1',  
+    version = '1.1.0',  
     description = 'A python package for working with crystal orientations in transmission electron microscopy',
     long_description=long_description,
     long_description_content_type="text/markdown",
